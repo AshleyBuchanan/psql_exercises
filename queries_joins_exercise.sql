774db40c217f5ebc77b1ec3179db3c5d50adc995
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-10-17 10:54:19 -0700

    deleted unneeded parentheses

--- queries_joins_exercise.sql
+++ queries_joins_exercise.sql
@@ -10,7 +10,7 @@ ON o.id = v.owner_id;
 SELECT first_name, last_name, COUNT(owner_id) 
 FROM owners o 
 JOIN vehicles v on o.id = v.owner_id 
-GROUP BY (first_name, last_name)
+GROUP BY first_name, last_name
 ORDER BY first_name;
 
 
